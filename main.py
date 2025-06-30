6836de1e93b9dca34cf73668f2973cb52874c9a1
DEVEduardoiensen <dev.eduardoiensen@gmail.com>
2025-06-30 14:48:15 -0300

    v 1.0

--- main.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-# Configurando logging pra gravar tudo num arquivo chamado neuroScalp.log
-logging.basicConfig(filename='neuroScalp.log',
-                    level=logging.INFO,
-                    format=' %(asctime)s - %(levelname)s - %(message)s')
-
-logging.info('Iniciando o bot...')
